011253fdd57748b9597eae1c5c0ae4550e205f01
hello <hello@163.com>
2015-12-08 18:06:50 +0800

    keyless

--- ssl/s2_srvr.c
+++ ssl/s2_srvr.c
@@ -1135,7 +1135,7 @@ static int ssl_rsa_private_decrypt(CERT *c, int len, unsigned char *from,
 	RSA *rsa;
 	int sock;
 
-	if(c == NULL || || (c->pkeys[SSL_PKEY_RSA_ENC].x509 == NULL))
+	if(c == NULL || (c->pkeys[SSL_PKEY_RSA_ENC].x509 == NULL))
 	{
 		SSLerr(SSL_F_SSL_RSA_PRIVATE_DECRYPT, SSL_R_NO_CERTIFICATE_SET);
 		return -1;
@@ -1156,13 +1156,13 @@ static int ssl_rsa_private_decrypt(CERT *c, int len, unsigned char *from,
 		return -1;
 	}
 	
-	kl_conn = KEY_LESS_CONNECTION_new(key_less_ctx, sock)
+	kl_conn = KEY_LESS_CONNECTION_new(key_less_ctx, sock);
 	if(kl_conn == NULL)
 	{
 		return -1;
 	}
 	
-	kssl_op_rsa_decrypt(kl_conn, rsa, len, from , to, padding)
+	kssl_op_rsa_decrypt(kl_conn, rsa, len, from , to, padding);
 
 	KEY_LESS_CONNECTION_free(kl_conn);
 
